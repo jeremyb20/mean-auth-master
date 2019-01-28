256f5cf71c1e9daabb964b9ffb1ee58a8f5c36ed
jbarquero <jbarquero@cog.cr>
2019-01-28 15:20:57 -0500

    My firts commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information 
+  database: 'mongodb://admin:admin123@ds135421.mlab.com:35421/mean_auth_mongo',   //prod
+  //mongodb://<dbuser>:<dbpassword>@ds135421.mlab.com:35421/mean_auth_mongo',    //dev
+  secret: 'yoursecret'
+}
