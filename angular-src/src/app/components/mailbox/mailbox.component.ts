0c5a58f5f244b92b42702d37b6f302f46366eea9
Jeremy <jbaccab@ucenfotec.ac.cr>
2020-03-29 23:32:22 -0600

    Add filter search and remove list user logged

--- angular-src/src/app/components/mailbox/mailbox.component.ts
+++ angular-src/src/app/components/mailbox/mailbox.component.ts
@@ -8,7 +8,13 @@ import { AuthService } from '../../services/auth.service';
 })
 export class MailboxComponent implements OnInit {
   user:Object;
-  allUsers: Object;
+  allUsers = [];
+  searchTerm;
+  itemsCopy;
+  items = this.itemsCopy;
+  itemUserSelected = [];
+  email:string;
+  elementSigned = [];
 
   constructor(
     private authService: AuthService
@@ -19,6 +25,7 @@ export class MailboxComponent implements OnInit {
       if(isLogged){
         this.authService.getProfile().subscribe(profile => {
           this.user = profile.user;
+          this.email = profile.user.email;
           this.getAllUsers();
       },
       err => {
@@ -28,11 +35,32 @@ export class MailboxComponent implements OnInit {
     }
   }
 
+  search(): void {
+    let term = this.searchTerm;
+    this.allUsers = this.itemsCopy.filter(function(tag) {
+        return tag.name.indexOf(term) >= 0;
+    }); 
+  }
+
+  test(val): void {
+    this.itemUserSelected.push(val);
+    if(this.itemUserSelected.length > 1){
+      this.itemUserSelected.shift();
+    }
+  }
+
   getAllUsers(){
     var isLoggedd = this.authService.loggedIn();
     if(isLoggedd){
       this.authService.getUsers().subscribe(yesy => {
         this.allUsers = yesy;
+        this.itemsCopy = yesy;
+        for (let i=0; i < this.allUsers.length; ++i) {
+          if (this.allUsers[i].email === this.email) {
+            this.allUsers.splice(i, 1); // Remove even numbers
+          }
+        }
+        this.itemUserSelected.push(yesy[0]);
     },
     err => {
       console.log(err);
