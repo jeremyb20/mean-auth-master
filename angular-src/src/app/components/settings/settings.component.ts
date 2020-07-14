2114b445eb9c36ec640cedff9e8778360211ceb7
jbarquero <jbaccab>
2020-07-14 14:14:14 -0600

    Add chat app test

--- angular-src/src/app/components/settings/settings.component.ts
+++ angular-src/src/app/components/settings/settings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
-import * as io from 'socket.io-client';
 import { environment } from '../../../environments/environment';
+import * as io from 'socket.io-client';
 
 
 @Component({
@@ -15,34 +15,68 @@ export class SettingsComponent implements OnInit {
   data: {};
   msgReceived = [];
 
-  private socket: any;
+  messageText: string;
+  messages: Array<any>;
+  avatar: string = 'https://api.adorable.io/avatars/40/abott@adorable.png';
+  selfAuthored: boolean = false;
+
+  //private socket: any;
 
   constructor( private chat: ChatService) { }
 
   ngOnInit() {
-    this.socket = io(environment.ws_url);
-    this.chat.messages.subscribe(msg => {
-      this.msgReceived.push(msg);
-      this.username = msg.username
-      // console.log(this.msgReceived);
-    })
+    // this.socket = io(environment.ws_url);
+    // this.chat.messages.subscribe(msg => {
+    //   this.msgReceived.push(msg);
+    //   this.username = msg.username
+    // })
+
+    this.messages = new Array();
+    this.chat.on('message-received',(msg:any)=> {
+      this.messages.push(msg);
+    });
+
+    // this.chat.emit('event1', {
+    //   msg:'Client to the server, can you fuck me?'
+    // });
+  
+    // this.chat.on('event2',(data:any)=> {
+    //   console.log(data.msg)
+    //   this.chat.emit('event3', {
+    //     msg: 'Yes its working for me'
+    //   });
+    // });
+  
+    //   this.chat.on('evet4', (data:any)=> {
+    //     console.log(data.msg);
+    //   });
   }
 
-  typing(val) {
-    console.log(val);
-    console.log(this.username);
-    this.socket.emit('typing',this.username);
+  // typing(val) {
+  //   console.log(val);
+  //   console.log(this.username);
+  //   this.chat.emit('typing',this.username);
 
-    this.socket.on('chat:typing',function(data){
-      this.actions.innerHTML = `<p><em>${data} is typing a message</em></p>`;
-    })
-  }
+  //   this.chat.on('chat:typing',function(data){
+  //     this.actions.innerHTML = `<p><em>${data} is typing a message</em></p>`;
+  //   })
+  // }
   
   sendMessage() {
-    this.data = {
-      username: this.username,
-      message: this.message
+    // this.data = {
+    //   username: this.username,
+    //   message: this.message
+    // }
+    // this.chat.sendMsg(this.data);
+
+    const message = {
+      text: this.messageText,
+      date: new Date(),
+      imageUrl: this.avatar
     }
-    this.chat.sendMsg(this.data);
+
+    this.chat.emit('send-message', message);
+    this.messageText ='';
+
   }
 }
--- angular-src/src/app/services/chat.service.ts
+++ angular-src/src/app/services/chat.service.ts
@@ -1,19 +1,46 @@
 import { Injectable } from '@angular/core';
 import { WebsocketService } from './websocket.service';
 import { Observable, Subject } from 'rxjs/Rx';
+import * as io from 'socket.io-client';
+import { eventNames } from 'cluster';
 
 @Injectable()
 export class ChatService {
+  socket: SocketIOClient.Socket;
 
   messages: Subject<any>
 
   constructor(private wsService : WebsocketService) { 
-    this.messages = <Subject<any>>wsService
-    .connect()
-    .map((response:any):any => {
-      return response;
-    })
+    // this.messages = <Subject<any>>wsService
+    // .connect()
+    // .map((response:any):any => {
+    //   return response;
+    // })
+
+    this.socket = io.connect();
   }
+
+
+
+  on(eventName: any, callback:any) {
+    if(this.socket){
+      this.socket.on(eventName, function(data:any){
+        callback(data);
+      });
+    }
+  };
+
+  emit(eventName: any, data:any) {
+    if(this.socket){
+      this.socket.emit(eventName,data);
+    }
+  };
+
+  removeListener(eventName: any) {
+    if(this.socket){
+      this.socket.removeListener(eventName);
+    }
+  };
 
   sendMsg(msg) {
     this.messages.next(msg);
